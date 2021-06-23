ba59dcd4dcef585ffb1935774b5044e8087ba837
Zameer-code <84001846+Zameer-code@users.noreply.github.com>
2021-06-23 16:14:27 +0530

    Writing Paramitrised test to validate the User Entries

--- src/main/java/com/UserRegistration.java
+++ src/main/java/com/UserRegistration.java
@@ -16,17 +16,39 @@ import java.util.regex.Pattern;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-		
-		/**
-		 * UC-10 to write J unit test for happy and sad test case
-		 * @param word
-		 * @return
-		 */
-	 public String happySad(String word) {
-	        if (word.contains("Negative"))
-	            return "Negative";
-	        else
-	            return "Positive";
-	    }
+    
+	
+    // UC1 - First name starts with Cap and has minimum 3 characters
+    public boolean firstName(String fN) {
+        return (Pattern.matches("[A-Z]{1}[a-z]{3,}", fN) == true);
+    }
 
+    // UC2 - Last name starts with Cap and has minimum 3 characters
+    public boolean lastName(String lN) {
+        return (Pattern.matches("[A-Z]{1}[a-z]{3,}", lN) == true);
+    }
+
+    /*
+     * UC 3 - enter a valid email UC 9 - Cleared All Sample Provided
+     */
+    public boolean email(String emailId) {
+        return (Pattern.matches("[a-zA-Z0-9][a-zA-Z0-9_.]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+", emailId) == true);
+    }
+
+    // UC4 - Mobile Format
+    public boolean mobile(String mobileNum) {
+        return(Pattern.matches("(0|91)?\\s{1}[6-9][0-9]{9}", mobileNum) == true);
+    }
+
+    // UC 5 to 8 - Password
+    public boolean password(String pass) {
+        return ((Pattern.matches("[A-Z0-9a-z$%&*@!#]{8,}", pass) && Pattern.matches(".*[A-Z].*", pass) && Pattern.matches(".*[0-9].*", pass) && Pattern.matches(".*[\\W]{1}.*", pass)) == true);
+    }
+
+    public String happySad(String word) {
+        if (word.contains("Sad"))
+            return "Sad";
+        else
+            return "Happy";
+    }
 }
